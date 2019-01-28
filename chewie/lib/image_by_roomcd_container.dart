0b9950ed6b67b38a451fa3f9410f7105fa53a7de
mrvu <vunq@i-com.vn>
2019-01-28 20:22:58 +0700

    [RFT] Change clip  url

--- chewie/lib/image_by_roomcd_container.dart
+++ chewie/lib/image_by_roomcd_container.dart
@@ -27,7 +27,9 @@ class _VideoAppState extends State<ImageByRoomCdContainer> {
   void initState() {
     super.initState();
     _controller = VideoPlayerController.network(
-        'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4')
+//        'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4'
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    )
       ..initialize().then((_) {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
